bcc3ad83dd9d1f4a219c70cf8645f71c07a604ef
AbdelmoulaElAmrani <elamraniabdelmoula@gmail.com>
2018-11-02 16:56:38 -0500

    click working

--- src/index.js
+++ src/index.js
@@ -17,33 +17,51 @@ getMovies().then((movies) => {
   let buildMoviesHTML = `<ul>`;
   movies.forEach(({title, rating, id}) => {
     console.log(`${title} - rating: ${rating}`);
-    buildMoviesHTML += `<li class="eachMovie">${title} - rating: ${rating}</li>`;
+    buildMoviesHTML += `<li class="eachMovie">${title} - rating: ${rating} <button class="deleteMovie"> X</button></li>`;
   });
   buildMoviesHTML += `</ul>`;
 
-  $('.moviesList').append(buildMoviesHTML + addMovie() + addRating());
-
+  $('.moviesList').append(buildMoviesHTML + addMovieHtml() + addRating());
+    deleteMovie()
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
 
-const addMovie = () => {
-  return `<div class="row">
-            <form class="col s12">
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="email" type="email" class="validate">
-                  <label for="email">Email</label>
-                </div>
-              </div>
-            </form>
-          </div>`
+const addMovieHtml = () => {
+  return `<form class="form-inline">
+
+  <label class="sr-only" for="inlineFormInputName2">Name</label>
+  <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Movie Title">
+
+
+  <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Rate your movie</label>
+  <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+    <option selected value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+    <option value="4">Four</option>
+    <option value="5">Five</option>
+
+  </select>
+  <button type="submit" class="btn btn-primary my-1">ADD</button>
+</form>`
 };
 
-const addRating = () => {
-  return `<div>Hey Pibo</div>`
+
+
+//delete movie
+const deleteMovie = () => {
+
+$('.deleteMovie').click( (event) => {
+    $(event.target).parent().hide();
+})
+
 };
+
+const addNewMovie = () => {
+
+}
 
 
 
