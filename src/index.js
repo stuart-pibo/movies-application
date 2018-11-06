973c73a33e420c0994ee37259792a6e41dab2759
Stuart Hamblin <stuart@stuarts-air.home>
2018-11-05 20:50:19 -0600

    added editHtml

--- src/index.js
+++ src/index.js
@@ -53,10 +53,34 @@ const addMovieHtml = () => {
 
   </select>
   <button class="btn addMovieButton my-1">ADD</button>
-</form>`
+</form>`;
     return $('.addMovie').html(form)
 };
-addMovieHtml()
+addMovieHtml();
+
+const editMovieHtml = (input) => {
+    console.log(input.title);
+    console.log(input.rating);
+    console.log(input.id);
+    const output = `<form class="form-inline">
+
+        <label class="sr-only" for="inlineFormInputName2">Name</label>
+        <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Movie Title" value=${input.title}>
+
+
+        <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Movie Rating</label>
+    <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+        <option selected value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+        <option value="4">Four</option>
+        <option value="5">Five</option>
+
+        </select>
+        <button class="btn addMovieButton my-1">UPDATE</button>
+        </form>`;
+    return output
+};
 
 
 //delete movie
@@ -85,7 +109,6 @@ $('.addMovieButton').click((event) => {
     };
     fetch(url, options)
         .then(
-            console.log('pass'),     ////update the displayed content here
             update()
         );
 });
@@ -112,23 +135,50 @@ const deleteMovie = () => {
 
 
 const editMovie = () => {
-    $('.editBtn').click(() => {
-        let clicked = $(event.target).parents('li').text();
-        let nameMovie = clicked.substring(0, clicked.length-15);
-        let  rating = clicked.substring(clicked.length-4 ,clicked.length-2 );
-        console.log(rating);
+    $('.editBtn').click( (event) => {
+        let clicked = $(event.target).parent().parent();
         let id = clicked.attr('id');
-        $.ajax(`/api/movies/${id}`, {
+        console.log(id);
+        // let nameMovie = clicked.substring(0, clicked.length-15);
+        // let  rating = clicked.substring(clicked.length-4 ,clicked.length-2 );
+        // console.log(rating);
+        // $('#7').css('background-color', 'yellow');
+        const getTheData = $.ajax(`/api/movies/${id}`, {
             method: 'GET',
             success: function (data) {
-                console.log(data)
+                const selector = '#' + data.id;
+                console.log(selector);
+                const newHtml = editMovieHtml(data);
+                $(selector).html(newHtml);
+
+
+
+            //     let editHTML =`<form class="form-inline">
+            //
+            //   <label class="sr-only" for="inlineFormInputName2">Name</label>
+            //   <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Movie Title">
+            //
+            //
+            //   <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Rate your movie</label>
+            //   <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+            //     <option selected value="data.id">One</option>
+            //     <option value="2">Two</option>
+            //     <option value="3">Three</option>
+            //     <option value="4">Four</option>
+            //     <option value="5">Five</option>
+            //
+            //   </select>
+            //   <button class="btn updateMovieButton my-1">UPDATE</button>
+            // </form>`;
+            //     return $('.addMovie').html(form)
             }
-        }).then()
-        ;
+        }).then();
+
+        // console.log(getTheData);
         update();
 
     })
-}
+};
 //
 // const theTargetMovie =;
 // const url = '/posts';
